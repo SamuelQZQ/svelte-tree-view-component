9e9eabec01d2833954fa7166eb45e433b0fe4035
Samuel Qian <Qian.Zheqi@microsoft.com>
2022-04-18 01:19:44 +0800

    edit readme

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 A beautiful and easy to use Tree View component for Svelte.
 
-![](./screeshot.gif)
+## **[REPL Demo](https://svelte.dev/repl/8b1d026ca6e845bc83bffa5781a519ca?version=3.47.0)**
+
+Screenshot:
+
+![](https://raw.githubusercontent.com/SamuelQZQ/svelte-tree-view-component/main/screeshot.gif)
 
 ## Install
 ```
@@ -10,4 +14,69 @@ npm i svelte-tree-view-component
 ```
 
 ## How to use
-[WIP]
+
+### Basic usage
+```jsx
+<TreeView >
+    <TreeBranch rootContent="Languages">
+        <TreeLeaf>JavaScript</TreeLeaf>
+        <TreeLeaf>C#</TreeLeaf>
+    </TreeBranch>
+    <TreeBranch rootContent="Frameworks">
+        <TreeLeaf>Svelte</TreeLeaf>
+        <TreeLeaf>React</TreeLeaf>
+    </TreeBranch>
+</TreeView>
+```
+
+### Nested branch
+```jsx
+<TreeView >
+    <TreeBranch rootContent="Languages">
+        <TreeLeaf>JavaScript</TreeLeaf>
+        <TreeBranch rootContent="Frameworks">
+            <TreeLeaf>Svelte</TreeLeaf>
+            <TreeLeaf>React</TreeLeaf>
+        </TreeBranch>
+    </TreeBranch>
+</TreeView>
+```
+
+### Customized leaf
+```jsx
+<TreeView >
+    <TreeBranch rootContent="Languages">
+        <TreeLeaf><div style="color:red;">Red</div></TreeLeaf>
+    </TreeBranch>
+</TreeView>
+```
+
+### Customized branch
+```jsx
+<TreeView >
+    <TreeBranch >
+        <div style="color:red;" slot="root">Red</div>
+        <svelte:fragment slot="children">
+            <TreeLeaf>Svelte</TreeLeaf>
+            <TreeLeaf>React</TreeLeaf>
+        </svelte:fragment>
+    </TreeBranch>
+</TreeView>
+```
+
+### Change colors/Theme
+```jsx
+<TreeView     
+    lineColor="#ff0000"
+    iconBackgroundColor="#ff0000"
+    iconColor="#000000"
+    branchHoverColor="#ff0000">
+    <TreeBranch rootContent="Languages">
+        <TreeLeaf>JavaScript</TreeLeaf>
+        <TreeLeaf>C#</TreeLeaf>
+    </TreeBranch>
+</TreeView>
+```
+
+## Reference
+- [Codepen: Pure CSS Tree Menu](https://codepen.io/bisserof/pen/nrMveb)
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,33 +2,49 @@
 	import { TreeBranch, TreeLeaf, TreeView } from '../lib/index';
 </script>
 
+<TreeView>
+    <TreeBranch rootContent="Languages">
+		<TreeLeaf>JavaScript</TreeLeaf>
+		<TreeLeaf>C#</TreeLeaf>
+	</TreeBranch>
+	<TreeBranch rootContent="Frameworks">
+		<TreeLeaf>Svelte</TreeLeaf>
+		<TreeLeaf>React</TreeLeaf>
+	</TreeBranch>
+</TreeView>
 <h1>Welcome to SvelteKit</h1>
-<TreeView lineColor="gray" iconBackgroundColor="gray" iconColor="white" branchHoverColor="blue">
+<TreeView>
 	<TreeBranch rootContent="Languages">
+		<TreeLeaf>JavaScript/Typescript</TreeLeaf>
 		<TreeLeaf>C#/.NET</TreeLeaf>
-		<TreeBranch>
-			<TreeLeaf>C#/.NET</TreeLeaf>
+		<TreeBranch rootContent="Rust">
+			<TreeLeaf>project</TreeLeaf>
 			<TreeLeaf>
-				<span class="tree_label">C#/.NET</span>
+				<span style="color:red">project 2</span>
 			</TreeLeaf>
-			<TreeBranch rootContent="Languages">
-				<TreeLeaf>C#/.NET</TreeLeaf>
-				<TreeBranch>
-					<TreeLeaf>C#/.NET</TreeLeaf>
-					<TreeLeaf>
-						<span class="tree_label">C#/.NET</span>
-					</TreeLeaf>
-				</TreeBranch>
-				<TreeLeaf><span class="tree_label">C#/.NET</span></TreeLeaf>
+			<TreeBranch rootContent="project 3">
+				<TreeLeaf>Detail 1</TreeLeaf>
+				<TreeLeaf>Detail 2</TreeLeaf>
 			</TreeBranch>
 		</TreeBranch>
-		<TreeLeaf><span class="tree_label">C#/.NET</span></TreeLeaf>
+		<TreeLeaf>C++</TreeLeaf>
 	</TreeBranch>
-	<TreeBranch defaultClosed>
-		<TreeLeaf><span class="tree_label">C#/.NET</span></TreeLeaf><TreeLeaf
-			><span class="tree_label">C#/.NET</span></TreeLeaf
-		><TreeLeaf><span class="tree_custom">Specified tree item view</span></TreeLeaf>
+	<TreeBranch defaultClosed rootContent="Close by default">
+		<TreeLeaf>Svelte</TreeLeaf>
+		<TreeLeaf>Vue</TreeLeaf>
+		<TreeLeaf>React</TreeLeaf>
 	</TreeBranch>
 </TreeView>
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://github.com/SamuelQZQ/svelte-tree-view-component"
+		>github.com/SamuelQZQ/svelte-tree-view-component</a
+	> to read the documentation
+</p>
+
+<style>
+	:global(body) {
+		background: #2a2a2a;
+		color: white;
+	}
+</style>
